73e6e8d007cb645db8545e33b2346a83ef5ea988
Juan UPB Linux kraken <juanf.franco@upb.edu.co>
2022-08-01 16:02:12 -0500

    add multiTask support

--- multiTasks.ino
+++ multiTasks.ino
@@ -1,9 +1,17 @@
+#include "task1.h"
+#include "task2.h"
+#include "task3.h"
+
 void setup()
 {
-	
+    task1();
+    task2();
+    task3();
 }
 
 void loop()
 {
-	
+    task1();
+    task2();
+    task3();
 }
