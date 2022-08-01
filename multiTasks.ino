1649df21f76e19475068c10f4585fefa01b6cc20
Juan UPB Linux kraken <juanf.franco@upb.edu.co>
2022-08-01 16:22:03 -0500

    Add task4

--- multiTasks.ino
+++ multiTasks.ino
@@ -1,12 +1,14 @@
 #include "task1.h"
 #include "task2.h"
 #include "task3.h"
+#include "task4.h"
 
 void setup()
 {
     task1();
     task2();
     task3();
+    task4();
 }
 
 void loop()
@@ -14,4 +16,5 @@ void loop()
     task1();
     task2();
     task3();
+    task4();
 }
---
+++ task4.cpp
@@ -0,0 +1,8 @@
+#include <Arduino.h>
+#include "task43.h"
+
+
+void task4(){
+
+    
+}
